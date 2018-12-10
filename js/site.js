7ec094aa7d02f85ccc247830761477de7d7c6aa9
Ted Matuszewski <ted.matuszewski@outlook.com>
2018-12-09 20:51:00 -0500

    Added null check to hashchange function.

--- js/site.js
+++ js/site.js
@@ -1,6 +1,11 @@
 ; (function() {
     $( window ).on('hashchange', function( e ) {
         var id = window.location.hash;
+
+        if(id == null || id == "") {
+            return false;
+        }
+
         var to = $("[data-page='" + id +"']").offset().top - 55;
 
         $('html, body').stop().animate({
